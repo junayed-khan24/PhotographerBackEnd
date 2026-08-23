00cf3dc3980138ea644acd295de26e411c834ede
Junayed <junayed@example.com>
2026-08-24 01:05:34 +0600

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Photographer Backend is running!");
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000");
+});

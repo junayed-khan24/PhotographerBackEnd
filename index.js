f9e5300057afee85876fa4677539ffe26f335387
Junayed <junayed@example.com>
2026-08-24 20:43:40 +0600

    added items

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+import { MongoClient } from 'mongodb';
+
 
 const app = express();
+const port = process.env.PORT || 5000;
 
+// MIDILEware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const client = new MongoClient("mongodb+srv://<db_username>:<db_password>@cluster0.i9yr5cu.mongodb.net/?appName=Cluster0");
+
+export async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("You successfully connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+// Call this only when your application terminates
+export async function disconnectFromMongoDB() {
+  await client.close();
+}
+
+
 
 app.get("/", (req, res) => {
   res.send("Photographer Backend is running!");

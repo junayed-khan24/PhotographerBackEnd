368ff0998ee2c71520382d21c3007efb2ecf2668
Junayed <freelancerjunayedjunayed@gmail.com>
2026-08-25 16:42:09 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
-import { MongoClient } from 'mongodb';
+const { MongoClient } = require("mongodb");
+
+dotenv.config();
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// MIDILEware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB
+const client = new MongoClient(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i9yr5cu.mongodb.net/?appName=Cluster0`
+);
 
-
-
-
-const client = new MongoClient("mongodb+srv://:<db_password>@cluster0.i9yr5cu.mongodb.net/?appName=Cluster0");
-
-export async function connectToMongoDB() {
+async function connectToMongoDB() {
   try {
     await client.connect();
-    console.log("You successfully connected to MongoDB!");
+    console.log("Successfully connected to MongoDB!");
     return client;
   } catch (err) {
-    console.dir(err);
+    console.error("MongoDB connection error:", err);
   }
 }
 
-// Call this only when your application terminates
-export async function disconnectFromMongoDB() {
+async function disconnectFromMongoDB() {
   await client.close();
 }
 
-
-
+// Route
 app.get("/", (req, res) => {
   res.send("Photographer Backend is running!");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000");
+// Start server
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+
+  await connectToMongoDB()
 });

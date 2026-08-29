fe9a2dfce2bc8fc30c169f19e216e40bbc8b1715
Junayed <freelancerjunayedjunayed@gmail.com>
2026-08-29 21:55:41 +0600

    added post

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { MongoClient } = require("mongodb");
 
 dotenv.config();
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,28 +17,49 @@ const client = new MongoClient(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i9yr5cu.mongodb.net/?appName=Cluster0`
 );
 
-async function connectToMongoDB() {
+async function run() {
   try {
     await client.connect();
+
     console.log("Successfully connected to MongoDB!");
-    return client;
+
+    // Database
+    const database = client.db("photographerDB");
+
+    // Collection
+    const bookingsCollection = database.collection("bookings");
+
+    // Home Route
+    app.get("/", (req, res) => {
+      res.send("Photographer Backend is running!");
+    });
+
+    // Create Booking
+    app.post("/bookings", async (req, res) => {
+      try {
+        const booking = req.body;
+
+        booking.createdAt = new Date();
+
+        const result = await bookingsCollection.insertOne(booking);
+
+     
+      } catch (error) {
+        console.error(error);
+
+      }
+    });
+
+   
+
   } catch (err) {
     console.error("MongoDB connection error:", err);
   }
 }
 
-async function disconnectFromMongoDB() {
-  await client.close();
-}
-
-// Route
-app.get("/", (req, res) => {
-  res.send("Photographer Backend is running!");
-});
+run();
 
 // Start server
-app.listen(port, async () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-
-  await connectToMongoDB()
 });
